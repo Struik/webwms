6df1f44a6e067d49d195100806e2066219b410bd
Struik <struikol@gmail.com>
2014-05-04 12:45:46 +0400

    ajax stuff

--- wms/views.py
+++ wms/views.py
@@ -38,10 +38,11 @@ def order(request):
     return render_to_response('wms/order.html', {'available_orders':available_orders, 'available_order_details':available_order_details}, context_instance=RequestContext(request))
 
 def order_detail(request):
+    message = "0"
     if request.is_ajax():
-        message = json.loads(request.body)
+        message = "123!"
     else:
-        message = json.loads(request.body)
+        message = "Not Ajax"
     return HttpResponse(message)
 
 @login_required
